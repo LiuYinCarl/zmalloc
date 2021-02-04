c016300ef42f2350064c22f59ab1ee36ec7f42b6
root <root@localhost.localdomain>
2021-02-04 14:35:24 +0800

    修复部分语法错误

--- zmalloc.h
+++ zmalloc.h
@@ -1,17 +1,19 @@
 #pragma once
 
+#include <stdio.h>
+#include <memory.h>
+#include <string.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <sys/mman.h>
-#include <memory.h>
 
 
 #define ZDEBUG
 
 #ifdef ZDEBUG
-#define zassert(cond) assert(cond)
+#define zassert(cond) \
+  assert(cond)
 #define zlog(format, ...) \
   printf("file:%s, line:%d: %s\n", __FILE__, __LINE__, format, ##__VA_ARGS__)
 #else
@@ -19,45 +21,28 @@
 #define zlog(format, ...)
 #endif
 
-// --------------------------------------------------------------------
-#define max_alloc_size 1024 - 8
-#define init_page_num 10
-#define alloc_page_num 10
-#define page_size 1024
-#define header_size 8
-#define ptr_size 8
+#define zcast(t, exp)	((t)(exp))
+
+#define max_alloc_size  1024 - 8
+#define init_page_num   10
+#define alloc_page_num  10
+#define page_size       1024
+#define header_size     8
+#define ptr_size        8
 
 const static int32_t slot_count = 32;
-const static int32_t slot_size[] = {
+const static int32_t slot_size[32] = {
   24,  56,  88,  120, 152, 184, 216, 248,
   280, 312, 344, 376, 408, 440, 472, 504,
   536, 568, 600, 632, 664, 696, 728, 760,
   792, 824, 856, 888, 920, 952, 984, 1016 };
 
-
-
 // -------------- Znode -------------------------
-
 typedef struct Znode {
   struct Znode* prev;
   struct Znode* next;
   void* val;
 } Znode;
-
-inline int32_t is_linked(Znode* node) {
-  assert(node);
-  return (node->prev || node->prev);
-}
-
-inline void remove_self(Znode* node) {
-  zassert(node);
-  if (node->prev) node->prev->next = node->next;
-  if (node->next) node->next->prev = node->prev;
-  node->next = NULL;
-  node->prev = NULL;
-}
-
-// -------------- Zlist -------------------------
 
 typedef struct Zlist {
   Znode* head;
@@ -65,47 +50,65 @@ typedef struct Zlist {
   int32_t len;
 } Zlist;
 
-static inline void list_init(Zlist* list) {
-  Znode* fakenode;
-  fakenode->prev = NULL;
-  fakenode->next = NULL;
-  fakenode->val = NULL;
+static inline int32_t node_is_linked(Znode* node) {
+  zassert(node);
+  return (node->prev || node->prev);
+}
 
-  list->head = fakenode;
-  list->tail = fakenode;
-  list->len = 0;  // fakenode is not a node
+static inline void node_remove_self(Znode* node) {
+  zassert(node);
+  if (node->prev) node->prev->next = node->next;
+  if (node->next) node->next->prev = node->prev;
+  node->next = NULL;
+  node->prev = NULL;
+}
+
+static inline Znode* node_get_next(Znode* node) {
+  zassert(node);
+  if (node->next)
+    return node->next;
+  else
+    return NULL;
+}
+
+static inline void list_init(Zlist* list) {
+  list->head = NULL;
+  list->tail = NULL;
+  list->len = 0;
 }
 
 static inline void list_clear(Zlist* list) {
   zassert(list);
-  Znode* node = list->head->next;  // list's first node is fakenode
+  Znode* node = list->head;
   Znode* next = NULL;
   while (node) {
     next = node->next;
     free(node);
     node = next;
   }
+  list->head = NULL;
+  list->tail = NULL;
   list->len = 0;
 }
 
 static inline void list_add_head(Zlist* list, Znode* node) {
   zassert(list);
   zassert(node);
-  if (!list->head->next) { // empty list
-    list->head->next = node;
+  if (!list->head) { // empty list
+    list->head = node;
     list->tail = node;
   } else {
-    node->next = list->head->next;
-    list->head->next->prev = node;
-    list->head->next = node;
+    node->next = list->head;
+    list->head->prev = node;
+    list->head = node;
   }
 }
 
 static inline void list_add_tail(Zlist* list, Znode* node) {
   zassert(list);
   zassert(node);
-  if (!list->tail->prev) { // empty list
-    list->head->next = node;
+  if (!list->tail) { // empty list
+    list->head = node;
     list->tail = node;
   } else {
     list->tail->next = node;
@@ -116,16 +119,16 @@ static inline void list_add_tail(Zlist* list, Znode* node) {
 
 static inline void* list_remove_head(Zlist* list) {
   zassert(list);
-  zassert(list->head->next);
-  Znode* node = list->head->next;
+  zassert(list->head);  // must at least one node on list
+  Znode* node = list->head;
   if (node->next) {  // more than one node on list
-    list->head->next = node->next;
-    node->next->prev = list->head;
+    list->head = node->next;
+    node->next->prev = NULL;
   } else { // only one node on list
-    list->head->next = NULL;  // todo
+    list->head = NULL;  // todo
     list->tail = NULL;
   }
-
+  --list->len;
   node->prev = node->next = NULL;
   return node;
 }
@@ -137,21 +140,19 @@ static inline void list_remove_tail(Zlist* list) {
 static inline void list_remove_node(Zlist* list, Znode* node) {
   zassert(list);
   zassert(node);
-  remove_self(node);
+  node_remove_self(node);
   --list->len;
 }
 
-// ---------------page alloc/free --------------------------
 void* free_page_list[alloc_page_num];
 size_t free_page_pos = -1;
 
-#define allocmem() /
-  mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)
+#define allocmem() \
+  mmap(NULL, page_size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0)
 
-#define freemem(ptr) /
+#define freemem(ptr) \
   munmap(ptr, page_size)
 
-// ---------------------------------------------
 #define blocks_per_page(page_size, block_size) \
   (page_size / block_size)  // todo: 优化除法
 
@@ -163,8 +164,8 @@ static inline size_t next_blocksize(size_t size) {
 }
 
 static inline size_t get_pos_by_blocksize(size_t blockSize) {
-  mem_assert(blockSize > 0);
-  mem_assert(blockSize % 32 == 0);
+  zassert(blockSize > 0);
+  zassert(blockSize % 32 == 0);
   int n = 0;
   int bs = blockSize;
   bs = bs >> 6;  // bs = bs / 32
@@ -175,8 +176,14 @@ static inline size_t get_pos_by_blocksize(size_t blockSize) {
   return n;
 }
 
+typedef struct Zslab {
+  Zlist* pagelist;
+  size_t pagenum;
+  Zpage* minfreepage;
+  Zlist* fulllist;
+  Zlist* freelist;
+} Zslab;
 
-// -------------------------------------------------------------------
 typedef struct Zpage {
   Znode* selfnode;
   void* ppage;
@@ -188,12 +195,6 @@ typedef struct Zpage {
   size_t hasspace;
   Zslab* slab;
 } Zpage;
-
-typedef struct Zslab {
-  Zlist* pagelist;
-  size_t pagenum;
-  Zslot* minfreeslot;
-} Zslab;
 
 typedef struct Zpool {
   Zslab slabs[slot_count];
@@ -221,33 +222,32 @@ static inline void page_init(Zpage* page, Zslab* slab, size_t blocksize) {
   page->blocknum = blocks_per_page(page_size, blocksize);
   page->slab = slab;
 
-  char* block = page->ppage;
-  page->stack = malloc(page->blocknum * sizeof(void*));
+  char* block = zcast(char*, page->ppage);
+  page->stack = zcast(void**, malloc(page->blocknum * sizeof(void*)));
   for (size_t i = 0; i < page->blocknum; ++i) {
     void* p = (block + i * blocksize);
     page->stack[i] = set_block_header(p, page);
   }
-
   page->tailpos = page->blocknum - 1;
   page->hasspace = 1;
 }
 
-static inline page_uninit(Zpage* page) {
+static inline void page_uninit(Zpage* page) {
   zassert(page);
   freemem(page->ppage);
-  free(page->stack)
+  free(page->stack);
 }
 
 static inline void* page_alloc(Zpage* page) {
   zassert(page->tailpos >= 0);
-  zassert(page.tailpos < page->blocknum);
+  zassert(page->tailpos < page->blocknum);
   void* ptr = page->stack[page->tailpos];
 
   if (--page->tailpos < 0) {
     page->hasspace = false;
     Znode* node = page->slab->freelist.removehead();
     page->slab->fulllist.addtail();
-    slab_find_min_freeslot(page->slab);
+    slab_find_min_freepage(page->slab);
   }
   return ptr;
 }
@@ -261,7 +261,6 @@ static inline void page_free(Zpage* page, void* ptr) {
   // todo memset the block
 }
 
-// ----------------------------------------------------------------
 
 static inline void slab_init(Zslab* slab, size_t blocksize) {
   slab->pagelist = malloc(sizeof(Zlist));
@@ -274,7 +273,7 @@ static inline void slab_uninit(Zslab* slab) {
   Znode* node = slab->pagelist->head->next;
   Zpage* page = NULL;
   while (node) { // free pages
-    page = node->val;
+    page = zcast(Zpage*, node->val);
     page_uninit(page);
     node = node->next;
   }
@@ -282,14 +281,14 @@ static inline void slab_uninit(Zslab* slab) {
   free(slab->pagelist);
 }
 
-static inline void slab_new_page(Zslab* slab, size_t blocksize) {
-  Zpage* page = malloc(sizeof(Zpage));
-  page_init(&page, slab, blocksize);
-  Znode* node = malloc(sizeof(Znode));
+static inline void slab_new_page(Zslab* slab) {
+  Zpage* page = zcast(Zpage*, malloc(sizeof(Zpage)));
+  page_init(page, slab, slab->blocksize);
+  Znode* node = zcast(Znode*, malloc(sizeof(Znode)));
   node->prev = NULL;
   node->next = NULL;
   node->val = page;
-  list_head_insert(slab->pagelist, node);
+  list_add_head(slab->pagelist, node);
   page->selfnode = node;
 }
 
@@ -301,20 +300,20 @@ static inline void* slab_alloc(Zslab* slab) {
   
 }
 
-static inline void find_min_freeslot(Zslab* slab) {
+static inline void slab_find_min_freepage(Zslab* slab) {
   zassert(slab);
   Zlist* freelist = slab->freelist;
   Zlist* fulllist = slab->fulllist;
   Zpage* minfreepage = NULL;
   if (freelist->len > 0) {
-    minfreepage = freelist->head->val;
+    minfreepage = zcast(Zpage*, freelist->head->val);
   } else {
     Znode* node = NULL;
     Znode* next = NULL;
     node = fulllist->head;
     while (node) {
       next = node_get_next(node);
-      if (node->val->hasspace) {
+      if (zcast(Zpage*, node->val)->hasspace) {
         node_remove_self(node);
         list_add_head(freelist, node);
       }
@@ -324,14 +323,9 @@ static inline void find_min_freeslot(Zslab* slab) {
       slab_new_page(slab);
     }
     zassert(freelist->len > 0);
-    slab->minfreeslot = list_get_head(freelist)->val;
+    slab->minfreepage = zcast(Zpage*, freelist->head->val);
   }
 }
-
-// ----------------------------------------------------------------
-
-
-
 
 static inline void zinit() {
   for (size_t i = 0; i < slot_count; ++i) {
@@ -344,7 +338,6 @@ static inline void zuninit() {
     slab_uninit(&(pool.slabs[i]), slot_size[i]);
   }
 }
-
 
 static inline void* zalloc(size_t size) {
   zassert(size >= 0);
@@ -369,22 +362,20 @@ static inline void zfree(void* ptr) {
 static inline void* zrealloc(void* ptr, size_t size) {
   zassert(ptr);
   zassert(size >= 0);
-  Zpage* page = get_block_header(ptr);
+  Zpage* page = zcast(Zpage*, get_block_header(ptr));
   if (size <= page->usersize) {
     return ptr;
   } else { // expand
     void* newptr = zalloc(size);
-    mmecpy(newptr, ptr, page->usersize);
+    memcpy(newptr, ptr, page->usersize);
     page_free(page, ptr);
     return newptr;
   }
 }
 
-// ---------------------------------------------------------------
 static inline void* z_lalloc(void* ud, void* ptr, size_t osize, size_t nsize) {
   (void)ud;
   static void* p = NULL;
-
 
   if (nsize == 0) { // free
     if (ptr == NULL) return NULL;
